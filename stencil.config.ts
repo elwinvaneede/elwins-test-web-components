98c8ca7aea2573394c113875f2033262e2881c80
Elwin van Eede <elwinvaneede@gmail.com>
2020-02-16 14:33:11 +0100

    Added react output target

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,13 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'elwins-test-web-components',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'elwins-test-web-components',
+      proxiesFile: '../elwins-test-web-components-react/src/components.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
