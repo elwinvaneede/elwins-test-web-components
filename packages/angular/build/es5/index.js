1035cc5338e0571221db9dff113ebc4873b6298c
Elwin van Eede <elwinvaneede@gmail.com>
2020-10-24 12:18:51 +0200

    Remove angular/build from repo and ignore

--- packages/angular/build/es5/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-// DIRECTIVES
-export { EveButton, EveIntroduction } from "./directives/proxies";
-// PACKAGE MODULE
-export { ElwinsTestWebComponentsModule } from "./component-library-module";
